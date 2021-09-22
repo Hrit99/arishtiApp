9859ce3dac07b1b3097c2e80e24ea27a69c5a09a
Hrit99 <hritikpihal51299@gmail.com>
2021-09-22 13:18:51 +0530

    deploying on heroku

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 var mongoose = require('mongoose');
-  
+
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const userRoute = require('./routes/userRoute')
 
 var mongoDB = 'mongodb+srv://hritik:hritikpassword@cluster0.jtmil.mongodb.net/users?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
 
 
 db.once('open', () => {
     console.log('Database connected');
-  })
-  
+})
+
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('connection', (stream) => {
     console.log('someone connected!');
-  });
+});
 
 
-  const app = express()
+const app = express()
 
-  app.use(morgan('dev'))
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-  const port = process.env.PORT||3000
 
-app.listen(port, () => {
-  console.log('server is running')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running')
 })
 
 app.use('/user', userRoute)
