718ce0abbdb51983b49e17d3c406c56d80b30766
Hrit99 <hritikpihal51299@gmail.com>
2021-09-22 13:01:59 +0530

    deploying on heroku

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ db.on('connection', (stream) => {
   app.use(bodyParser.urlencoded({extended:true}))
   app.use(bodyParser.json())
 
-  const port = process.env.PORT
+  const port = process.env.PORT||3000
 
 app.listen(port, () => {
   console.log('server is running')
